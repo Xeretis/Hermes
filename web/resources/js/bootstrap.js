2b2cdff5159499517fdcb6351f2d23aab3963612
Restyled.io <commits@restyled.io>
2024-02-28 11:01:02 +0000

    Restyled by clang-format

--- web/resources/js/bootstrap.js
+++ web/resources/js/bootstrap.js
@@ -4,9 +4,8 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 import axios from 'axios';
-
-import Pusher from 'pusher-js';
 import Echo from "laravel-echo";
+import Pusher from 'pusher-js';
 
 window.axios = axios;
 
@@ -21,17 +20,19 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = Pusher;
 
 let echoConfig = {
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+  broadcaster : 'pusher',
+  key : import.meta.env.VITE_PUSHER_APP_KEY,
+  cluster : import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+  wsHost : import.meta.env.VITE_PUSHER_HOST
+               ? import.meta.env.VITE_PUSHER_HOST
+               : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+  forceTLS : (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+  enabledTransports : [ 'ws', 'wss' ],
 };
 
 if (import.meta.env.VITE_PUSHER_PORT) {
-    echoConfig.wsPort = import.meta.env.VITE_PUSHER_PORT;
-    echoConfig.wssPort = import.meta.env.VITE_PUSHER_PORT;
+  echoConfig.wsPort = import.meta.env.VITE_PUSHER_PORT;
+  echoConfig.wssPort = import.meta.env.VITE_PUSHER_PORT;
 }
 
 window.Echo = new Echo(echoConfig);
